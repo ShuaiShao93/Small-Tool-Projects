af01c4d20269274d6e0ebbc3412c01f3275005ba
Shuai Shao <shuaishao93@gmail.com>
2024-03-16 20:06:04 -0700

    Update amazon.js

--- life_utils/amazon.js
+++ life_utils/amazon.js
@@ -1,16 +1,16 @@
-function deleteSavedItems() {
+function emptyCart() {
 	var query = document.querySelectorAll("#sc-active-cart input[value=Delete]")
 	if (query.length) {
 		query[0].click();
 	}
 	if (query.length > 1) {
-		setTimeout(deleteSavedItems,100);
+		setTimeout(emptyCart,100);
 	}
 	else {
 		console.log('Finished');
 	}
 }
-deleteSavedItems();
+emptyCart();
 
 
 function moveWishlistToCart() {
@@ -26,3 +26,16 @@ function moveWishlistToCart() {
 	}
 }
 moveWishlistToCart();
+
+
+function WishlistSumPrices() {
+	var priceSpans = document.querySelectorAll("#wl-item-view .a-price .a-offscreen")
+	var totalSum = 0;
+	priceSpans.forEach(priceSpan => {
+	            var price = parseFloat(priceSpan.textContent.replace('$', ''));
+	            totalSum += price;
+    	});
+	    
+	console.log('Total sum of all prices: $' + totalSum.toFixed(2));
+}
+WishlistSumPrices();
