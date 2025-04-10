078ac0686f44c1b09ba09f281a7944c0bace8219
Shuai Shao <shuaishao93@gmail.com>
2025-04-10 12:57:05 -0700

    Update model_packager.py

--- ml_platform/model_manager/model_packager.py
+++ ml_platform/model_manager/model_packager.py
@@ -2,8 +2,9 @@ import argparse
 import logging
 import os
 import tempfile
+from typing import List, Tuple
 
-from deploy.models import ModelArtifactConfig
+from deploy.models import CustomerModelConfig, ModelArtifactConfig
 from deploy.utils import (
     build_model_dir,
     md5_directory,
@@ -12,6 +13,31 @@ from deploy.utils import (
     parse_customer_config,
     push_package_to_s3,
 )
+
+
+def _get_all_customer_paths_and_configs() -> List[Tuple[str, CustomerModelConfig]]:
+    customer_paths_and_configs = []
+    for root, dirs, files in os.walk("config/customers"):
+        for file in files:
+            if file.endswith(".yaml"):
+                path = os.path.join(root, file)
+                customer_config = parse_customer_config(path)
+                customer_paths_and_configs.append((path, customer_config))
+    return customer_paths_and_configs
+
+
+def _get_changed_customers(changed_files: List[str]) -> List[str]:
+    customer_paths_and_configs = _get_all_customer_paths_and_configs()
+    changed_customers = set()
+    for customer_path, customer_config in customer_paths_and_configs:
+        if customer_path in changed_files:
+            changed_customers.add(customer_config.name)
+            continue
+        for artifact in customer_config.artifacts:
+            for model in artifact.models:
+                if model.base in changed_files:
+                    changed_customers.add(customer_config.name)
+    return list(changed_customers)
 
 
 def _build_artifact_tarball(
@@ -33,7 +59,7 @@ def _build_artifact_tarball(
     return md5_hash
 
 
-def package_customer(customer_name: str) -> None:
+def _package_customer(customer_name: str) -> None:
     logging.info(f"Packaging customer {customer_name}")
     customer_config = parse_customer_config(f"config/customers/{customer_name}.yaml")
     for artifact in customer_config.artifacts:
@@ -56,6 +82,13 @@ if __name__ == "__main__":
     logging.getLogger().setLevel(logging.INFO)
 
     arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument("--customer", type=str, required=True)
+    arg_parser.add_argument("--changed_files", type=str, nargs="+")
     args = arg_parser.parse_args()
-    package_customer(args.customer)
+
+    changed_customers = _get_changed_customers(args.changed_files)
+    logging.info(f"Changed customers: {changed_customers}")
+    if not changed_customers:
+        logging.info("No changed customers, skipping packaging")
+        exit(0)
+    for customer in changed_customers:
+        _package_customer(customer)
